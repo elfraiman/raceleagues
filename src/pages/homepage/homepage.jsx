d4689388e647642a38f3c55cf705d0e422566610
elfraim <elfraiman@gmail.com>
2020-08-03 13:12:26 +0200

    feat(homepage): animations for grid on scroll

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -10,6 +10,8 @@ import LeagueCard from '../../components/league-card/leagueCard';
 import TopNavBar from '../../components/navbar/navbar';
 import classes from './homepage.module.scss';
 import { useSpring, animated } from 'react-spring'
+import VisibilitySensor from "react-visibility-sensor";
+import { Spring } from "react-spring/renderprops";
 
 const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 60, 1.02]
 const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
@@ -17,6 +19,9 @@ const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg)
 const HomePage = () => {
   const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 0.5, tension: 250, friction: 20 } }))
   const fadeIn = useSpring({ opacity: 1, from: { opacity: 0 } })
+  function onChange(isVisible) {
+    console.log('Element is now %s', isVisible ? 'visible' : 'hidden');
+  }
 
   return (
     <React.Fragment>
@@ -58,24 +63,97 @@ const HomePage = () => {
       </div>
 
       <div className={classes.allRaces}>
+
         <div className={classes.racesTopLeft}>
-          <h3>
-            Meet fellow racers, make friends, race together and
-            help grow the online sim-racers community!
-          </h3>
+
+          <VisibilitySensor partialVisibility>
+            {({ isVisible }) => (
+              <Spring
+                delay={200}
+                to={{
+                  opacity: isVisible ? 1 : 0,
+                  transform: isVisible
+                    ? "translateY(0)"
+                    : "translateY(200px)",
+                }}
+              >
+                {(props) => (
+                  <h3 style={{ ...props }}>
+                    Meet fellow racers, make friends, race together and
+                    help grow the online sim-racers community!
+                  </h3>
+                )}
+              </Spring>
+
+            )}
+          </VisibilitySensor>
+
         </div>
 
-        <div className={classes.racesTopRight}>
-          <img src={spa3mixed} alt="spa3hours" />
-        </div>
+        <VisibilitySensor partialVisibility>
+          {({ isVisible }) => (
+            <Spring
+              delay={400}
+              to={{
+                opacity: isVisible ? 1 : 0,
+                transform: isVisible
+                  ? "translateY(0)"
+                  : "translateY(200px)",
+              }}
+            >
+              {(props) => (
+                <div style={{ ...props }} className={classes.racesTopRight}>
+                  <img src={spa3mixed} alt="spa3hours" />
+                </div>
+              )}
+            </Spring>
+          )}
+        </VisibilitySensor>
 
-        <div className={classes.racesBottomLeft}>
-          <img src={paulricard3gt3} alt="paulricard3hours" />
-        </div>
+        <VisibilitySensor partialVisibility>
+          {({ isVisible }) => (
+            <Spring
+              delay={300}
+              to={{
+                opacity: isVisible ? 1 : 0,
+                transform: isVisible
+                  ? "translateX(0px)"
+                  : "translateX(-200px)",
+              }}
+            >
+              {(props) => (
+                <div style={{ ...props }} className={classes.racesBottomLeft}>
+                  <img src={paulricard3gt3} alt="paulricard3hours" />
+                </div>
+              )}
+            </Spring>
+          )}
+        </VisibilitySensor>
 
-        <div className={classes.racesBottomRight}>
-          <iframe title="discord-widget" src="https://discordapp.com/widget?id=739559664458661960&theme=dark" width="350" height="400" allowtransparency="true" frameBorder="false" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-        </div>
+
+
+
+        <VisibilitySensor partialVisibility>
+          {({ isVisible }) => (
+            <Spring
+              delay={300}
+              to={{
+                opacity: isVisible ? 1 : 0,
+                transform: isVisible
+                  ? "translateX(0px)"
+                  : "translateX(200px)",
+              }}
+            >
+              {(props) => (
+                <div style={{ ...props }} className={classes.racesBottomRight}>
+                  <iframe title="discord-widget" src="https://discordapp.com/widget?id=739559664458661960&theme=dark" width="350" height="400" allowtransparency="true" frameBorder="false" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                </div>
+              )}
+            </Spring>
+          )}
+        </VisibilitySensor>
+
+
       </div>
     </React.Fragment>
   )
